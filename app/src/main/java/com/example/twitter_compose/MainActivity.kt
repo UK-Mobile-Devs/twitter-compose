3711f0d66f6f5bd0be2c5d3c32e1181a33cc77bb
oskarlasota <oskarlasota98@gmail.com>
2022-09-30 11:33:50 +0100

    adding decoration to the tweet component

--- app/src/main/java/com/example/twitter_compose/MainActivity.kt
+++ app/src/main/java/com/example/twitter_compose/MainActivity.kt
@@ -1,23 +1,26 @@
 package com.example.twitter_compose
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedButton
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.twitter_compose.ui.theme.Twitter_composeTheme
+import androidx.compose.ui.unit.sp
+import com.example.twitter_compose.feature.home.TweetModel
+import com.example.twitter_compose.ui.theme.TwitterTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,27 +36,32 @@ class MainActivity : ComponentActivity() {
 @Composable
 private fun App() {
     // A surface container using the 'background' color from the theme
-    Tweet("Tweeeit")
+    Tweet(TweetModel("Oskar Programming", "First compose tweet", null))
 }
 
 @Composable
-private fun Tweet(context: String) {
-    var expanded = remember { mutableStateOf(false) }
-    val verticalPadding = if(expanded.value) 48.dp else 0.dp
+private fun Tweet(tweet: TweetModel) {
     Surface(
         color = MaterialTheme.colors.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
-        Row(modifier = Modifier.padding(24.dp)) {
+        Row(modifier = Modifier.padding(12.dp)) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_baseline_accessibility_new_24),
+                contentDescription = "profile picture",
+                modifier = Modifier
+                    .padding(end = 12.dp)
+                    .size(40.dp)
+                    .clip(CircleShape)
+                    .border(2.5.dp, MaterialTheme.colors.secondary, CircleShape)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
             Column(
-                modifier = Modifier.weight(1f)
-                    .padding(bottom = verticalPadding)
+                modifier = Modifier
+                    .weight(1f)
             ) {
-                Text(text = "Hello, ")
-                Text(text = context)
-            }
-            OutlinedButton(onClick = { expanded.value = !expanded.value }) {
-                Text(text = (if (expanded.value) "Show Me More" else "Show me Less"))
+                Text(text = tweet.authorName, fontFamily = FontFamily.Monospace, modifier = Modifier.padding(bottom = 8.dp))
+                Text(text = tweet.context)
             }
         }
     }
@@ -61,9 +69,14 @@ private fun Tweet(context: String) {
 
 
 @Preview(showBackground = true)
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
 @Composable
 fun DefaultPreview() {
     TwitterTheme {
-        Tweet("Tweet")
+        Tweet(TweetModel("Oskar Brown", "First compose tweet", null))
     }
 }
---
+++ app/src/main/java/com/example/twitter_compose/feature/home/TweetModel.kt
@@ -0,0 +1,5 @@
+package com.example.twitter_compose.feature.home
+
+import java.util.*
+
+data class TweetModel(val authorName : String, val context : String, val date : Date?)
