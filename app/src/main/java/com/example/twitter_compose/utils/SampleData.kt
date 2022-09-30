6a7ad6da10d1ad534b0b0d0891d4bfe8e491fec4
oskarlasota <oskarlasota98@gmail.com>
2022-09-30 11:51:15 +0100

    adds sample data for tweets

---
+++ app/src/main/java/com/example/twitter_compose/utils/SampleData.kt
@@ -0,0 +1,40 @@
+package com.example.twitter_compose.utils
+
+import com.example.twitter_compose.feature.home.TweetModel
+
+object SampleData {
+
+    val tweetSample = listOf(
+        TweetModel(
+            "test1",
+            "tweet context 1",
+            null
+        ),
+        TweetModel(
+            "test2",
+            "tweet context 2",
+            null
+        ),
+        TweetModel(
+            "test3",
+            "tweet context 3",
+            null
+        ),
+        TweetModel(
+            "test4",
+            "tweet context 4",
+            null
+        ),
+        TweetModel(
+            "test5",
+            "tweet context 5",
+            null
+        ),
+        TweetModel(
+            "test6",
+            "tweet context 6",
+            null
+        ),
+    )
+
+}
