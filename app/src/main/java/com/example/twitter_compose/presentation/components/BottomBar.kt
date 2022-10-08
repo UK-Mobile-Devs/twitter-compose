647feb756482087be4a0a6f4f488597fae10ba4a
oskarlasota <oskarlasota98@gmail.com>
2022-10-08 18:47:11 +0100

    updating bottom bar colours

--- app/src/main/java/com/example/twitter_compose/presentation/components/BottomBar.kt
+++ app/src/main/java/com/example/twitter_compose/presentation/components/BottomBar.kt
@@ -1,17 +1,12 @@
 package com.example.twitter_compose.presentation.components
 
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.width
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Icon
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.currentBackStackEntryAsState
@@ -19,19 +14,24 @@ import com.example.twitter_compose.presentation.utils.BottomNavigation
 import com.example.twitter_compose.presentation.utils.Screen
 
 @Composable
-fun BottomBar(navController: NavController){
+fun BottomBar(navController: NavController) {
     val navBackStackEntry by navController.currentBackStackEntryAsState()
     val currentRoute = navBackStackEntry?.destination?.route
 
-    BottomNavigation(backgroundColor = Color(Color.White.value)) {
-        BottomNavigationItem(icon = { Icon(imageVector = Icons.Default.AddCircle, contentDescription = "Home") },
-            selectedContentColor = Color.Black,
-            unselectedContentColor = Color.White,
+    BottomNavigation(backgroundColor = Color(Color.LightGray.value)) {
+        BottomNavigationItem(icon = {
+            Icon(
+                imageVector = Icons.Default.AddCircle,
+                contentDescription = "Home"
+            )
+        },
+            //selectedContentColor = Color.Black,
+            //unselectedContentColor = Color.White,
             alwaysShowLabel = false,
             selected = currentRoute == Screen.HomeFeed.route,
             onClick = {
                 navController.navigate(Screen.HomeFeed.route) {
-                    popUpTo(navController.graph.findStartDestination().id){
+                    popUpTo(navController.graph.findStartDestination().id) {
                         saveState = true
                     }
                     launchSingleTop = true
@@ -39,8 +39,5 @@ fun BottomBar(navController: NavController){
                 }
             }
         )
-        Row {
-            Spacer(modifier = Modifier.width(56.dp))
-        }
     }
 }
