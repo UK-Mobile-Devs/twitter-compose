5848393c805e8dc1223a0a4af686d51e34e65974
oskarlasota <oskarlasota98@gmail.com>
2022-10-09 21:15:55 +0100

    setting up additional ui components in new tweet screen

--- app/src/main/java/com/example/twitter_compose/presentation/components/NewTweet.kt
+++ app/src/main/java/com/example/twitter_compose/presentation/components/NewTweet.kt
@@ -3,9 +3,10 @@ package com.example.twitter_compose.presentation.components
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -23,14 +24,15 @@ fun NewTweetScreen(navController: NavController) {
             Row(
                 modifier = Modifier
                     .padding(12.dp)
-                    .fillMaxWidth()
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
             ) {
                 Image(
                     painter = painterResource(id = R.drawable.ic_baseline_close_24),
                     contentDescription = "close screen",
                     modifier = Modifier
-                        .padding(end = 12.dp)
                         .size(24.dp)
+                        .align(Alignment.CenterVertically)
                         .clickable {
                             navController.navigate(Screen.HomeFeed.route) {
                                 popUpTo(Screen.HomeFeed.route) {
@@ -39,6 +41,28 @@ fun NewTweetScreen(navController: NavController) {
                             }
                         }
                 )
+                Button(onClick = { /*TODO*/ }, shape = RoundedCornerShape(30.dp)) {
+                    Text(text = "Tweet")
+                }
+            }
+            Row(
+                modifier = Modifier
+                    .padding(12.dp)
+                    .fillMaxWidth()
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_star),
+                    contentDescription = "profile picture",
+                    modifier = Modifier
+                        .size(24.dp)
+                )
+                Button(onClick = { /*TODO*/ }, shape = RoundedCornerShape(30.dp)) {
+                    Text(text = "Public")
+                }
+            }
+            Row(modifier = Modifier.padding(12.dp)) {
+                Spacer(modifier = Modifier.padding(24.dp))
+                TextField(value = "", onValueChange = {/*TODO*/}, modifier = Modifier.fillMaxWidth().fillMaxHeight())
             }
         }
     }
